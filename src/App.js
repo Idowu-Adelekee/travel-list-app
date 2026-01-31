e8683096eec925251f21bdedcd942c9ef1650d82
Idowu Adeleke <work2idowu@gmail.com>
2026-01-31 17:50:37 +0100

    Controlled element understood

--- src/App.js
+++ src/App.js
@@ -99,10 +99,10 @@ function Item({ item }) {
   );
 }
 
-// function Stats() {
-//   return (
-//     <footer className="stats">
-//       <em>💼You have X items on your list, and you already packed X (X%)</em>
-//     </footer>
-//   );
-// }
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
