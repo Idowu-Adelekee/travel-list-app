76b41a3a0065f295b0a039b68a2828072d8a347f
Idowu Adeleke <work2idowu@gmail.com>
2026-02-02 21:47:35 +0100

    Result

--- src/App.js
+++ src/App.js
@@ -168,3 +168,5 @@ const nuru = new Person("Nurudeen", 2004);
 const titi = new Person("Titilope", 2000);
 const bola = new Person("Adebola", 1982);
 const tinu = new Person("Tinuola", 2002);
+
+console.log(tinu);
