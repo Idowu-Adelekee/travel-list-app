ebf5bb3a9330facf4c854487dea0a0123cd6287f
Idowu Adeleke <work2idowu@gmail.com>
2026-02-21 22:43:20 +0100

    Basic clean up

--- src/components/App.js
+++ src/components/App.js
@@ -52,10 +52,3 @@ export default function App() {
     </div>
   );
 }
-
-// const idowu = "Idowu";
-// const noble = "Noble";
-// const adeleke = "Adeleke";
-// const adodotun = "Adedotun";
-// const idara = "Idara";
-// const glory = "Glory";
